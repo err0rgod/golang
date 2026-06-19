5d19df0c6221b06455ec9b75732124bc471f0a68
err0rgod <ndgaming458@gmail.com>
2026-06-19 14:16:49 +0530

    maps , slices

--- main.go
+++ main.go
@@ -4,11 +4,7 @@ package main
 import "fmt"
 
 func main(){
-	defer fmt.Print("Done exec")
-	sum := 0
-	for {
-		sum +=1
-		if (sum > 100) {break}
-		fmt.Println(sum)
-	}
+	defer fmt.Print("\nDone exec")
+	arr := [3]int{1, 2, 4}
+	fmt.Print(arr[1])
 }
