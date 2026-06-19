b3fea3698838ac45f3ed29589d3cd7495724a57b
err0rgod <ndgaming458@gmail.com>
2026-06-20 01:33:06 +0530

    q8  done

--- main.go
+++ main.go
@@ -4,21 +4,19 @@ import (
 	"fmt"
 )
 
-type User struct {
-	Name string
-	Age  int
+type Rectangle struct {
+	height float64
+	width float64
 }
 
-func isAdult(u User) bool {
-	if(u.Age >= 18) {return true}
-	return false
+func (r Rectangle) Area() float64 {
+	return r.height * r.width
 }
 
 func main(){
-	u := User{"alice",9}
-	v := User{"mdhmasale",19}
-	if (isAdult(u)) {fmt.Print("Chota bacha\n")
-	} else {fmt.Print("Bada Bachn\n")};
-	if (isAdult(v)) {fmt.Print("Chota Bacha\n")
-	} else {fmt.Print("Bada Bachn\n")}
+	u := Rectangle {
+		height: 10,
+		width: 20,
+	}
+	fmt.Print(u.Area())
 }
