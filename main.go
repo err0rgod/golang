bb4da667b7f10d681dd67c77387dff0c7027ee3e
err0rgod <ndgaming458@gmail.com>
2026-06-19 17:27:24 +0530

    q6

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
 )
 
 type User struct {
@@ -10,8 +9,16 @@ type User struct {
 	Age  int
 }
 
-func 
+func isAdult(u User) bool {
+	if(u.Age >= 18) {return true}
+	return false
+}
 
 func main(){
-
+	u := User{"alice",9}
+	v := User{"mdhmasale",19}
+	if (isAdult(u)) {fmt.Print("Chota bacha\n")
+	} else {fmt.Print("Bada Bachn\n")};
+	if (isAdult(v)) {fmt.Print("Chota Bacha\n")
+	} else {fmt.Print("Bada Bachn\n")}
 }
