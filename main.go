5d3bef10850c182293d7d7c6277ff508d3a7c6ea
err0rgod <ndgaming458@gmail.com>
2026-06-21 19:17:25 +0530

    basic interfaces

--- main.go
+++ main.go
@@ -1,25 +1,46 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type digit float64
 
-type Rectangle struct {
-	height digit
-	width digit
+type Shape interface{
+	Area() float64
 }
 
+type Rectangle struct{
+	height float64
+	width float64
 
-func (r Rectangle) Area() digit {
+}
+
+type Circle struct{
+	radius float64
+}
+
+type Triangle struct{
+	sidelength float64
+}
+
+func (r Rectangle) Area() float64 {
 	return r.height * r.width
 }
+func (c Circle) Area() float64 {
+	return c.radius* 2//for test using 2 as stale formula  
+}
+func (t Triangle) Area() float64{
+	return t.sidelength * 3
+}
 
-func main(){
-	u := Rectangle {
-		height: 10,
-		width: 20,
-	}
-	fmt.Print(u.Area())
+
+func printArea(s Shape) {
+	fmt.Print(s.Area(),"\n")
+}
+
+func main() {
+	r := Rectangle{10,10}
+	c:= Circle{10}
+	t:= Triangle{20}
+	printArea(r)
+	printArea(t)
+	printArea(c)
 }
