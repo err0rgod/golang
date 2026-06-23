3770d47a0dae0ec57ed151bdb1fe0b2fcabd60fc
err0rgod <ndgaming458@gmail.com>
2026-06-24 00:06:36 +0530

    wooah nare putma ve keda

--- interfaces.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main1()  {
-	fmt.Print("Hello")
-}
-
--- main.go
+++ main.go
@@ -1,46 +1,10 @@
 package main
 
+
 import "fmt"
 
 
-type Shape interface{
-	Area() float64
-}
-
-type Rectangle struct{
-	height float64
-	width float64
-
-}
-
-type Circle struct{
-	radius float64
-}
-
-type Triangle struct{
-	sidelength float64
-}
-
-func (r Rectangle) Area() float64 {
-	return r.height * r.width
-}
-func (c Circle) Area() float64 {
-	return c.radius* 2//for test using 2 as stale formula  
-}
-func (t Triangle) Area() float64{
-	return t.sidelength * 3
-}
-
-
-func printArea(s Shape) {
-	fmt.Print(s.Area(),"\n")
-}
-
 func main() {
-	r := Rectangle{10,10}
-	c:= Circle{10}
-	t:= Triangle{20}
-	printArea(r)
-	printArea(t)
-	printArea(c)
+	go fmt.Print("Better go")
+	fmt.Print("Hellow")
 }
