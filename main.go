21f2ab7356110349b320626334ad5511ffb65b9b
err0rgod <ndgaming458@gmail.com>
2026-06-27 21:55:44 +0530

    learned about taking user inputs

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
-
-import "fmt"
-
+import (
+	"fmt"
+)
 
 func main() {
-	go fmt.Print("Better go")
-	fmt.Print("Hellow")
+	var name string
+	var age int
+
+	fmt.Print("Enter name and age : ")
+	_,err := fmt.Scanf("%s %d",&name,&age)
+	if err != nil {
+		fmt.Print("Error reading input.")
+		return
+	}
+	fmt.Printf("Name : %s , age : %d ", name, age)
 }
