398a4c64d9fb74df2afa7ef7644e4579f0d3a01a
err0rgod <ndgaming458@gmail.com>
2026-06-27 22:31:18 +0530

    creating simple task manager

--- main.go
+++ main.go
@@ -2,14 +2,53 @@ package main
 
 import (
 	"fmt"
+
 )
+
+
 
 func main() {
 	taskmanager  := make(map[int]string)
-	taskmanager[1] ="do dishes"
-	taskmanager[2] = "wash clothes"
+	// create tasks in the map
 
-	for id,t := range taskmanager{
-		fmt.Printf("Task %d : %s\n",id,t)
+	for true {
+		option := 0
+		fmt.Print("Enter you choice-----\n")
+		fmt.Print("Enter 1 for adding a task.\n")
+		fmt.Print("Enter 2 for completing a task.\n")
+		fmt.Print("Enter 3 for listing all task.\n")
+		fmt.Print("Enter 4 for exiting.\n")
+		fmt.Scanf("%d", &option)
+		switch option{
+		case 1 :
+			fmt.Println("Enter the task id and task. (with spaces)")
+			var id int
+			var task string
+			
+			fmt.Scanln(&id,&task)
+			taskmanager[id] = task
+		case 2:
+			fmt.Print("Enter the task id to mark as completed.")
+			var id int
+			_,exists := taskmanager[id]
+			if exists {
+				delete(taskmanager,id)
+			} else {
+				fmt.Print("Invalid task ID.")
+			}
+		case 3 :
+			fmt.Print("Listing all tasks ----------")
+			for val,_ := range  taskmanager{
+				fmt.Println(val)
+			}
+		case 4:
+			return
+		default : 
+		fmt.Print("Select valid choice.")
+		
+		}
+		
+
 	}
+
 }
