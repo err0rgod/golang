226bbb5f9654f2e5a3e29760e9de49355fcbb665
err0rgod <ndgaming458@gmail.com>
2026-07-06 04:17:10 +0530

    created a broadcast server

--- main.go
+++ main.go
@@ -1,13 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var broadcast = make(chan []byte)	
+
+var Clients = make(map[*websocket.Conn]bool)
+
+func hub() {
+	for {
+		message := <- broadcast
+		for client := range Clients {
+			client.WriteMessage(websocket.TextMessage, message)
+		}
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	// adding clients
+	Clients[conn] = true
+	defer func(){
+		delete(Clients, conn)
+		conn.Close()
+	}()
+
+	// just reading messages 
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil{
+			return 
+		}
+		broadcast<- message
+	}
+
+	
+}
 
 func main() {
-	msgChan := make(chan string, 100)
-
-	msgChan <- "hello, how are you"
-	msgChan <- "i am fine"
-
-	fmt.Println(<-msgChan)
-	fmt.Println(<-msgChan)
+	go hub()
+	http.HandleFunc("/ws", wsHandler)
+	fmt.Println("Server chal raha hai :8080")
+	http.ListenAndServe(":8080", nil)
 }
